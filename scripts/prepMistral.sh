933f5ce44df3874d13b3fa8ed0b0828869b6385b
ayush-vatsal <ayushvatsal04@gmail.com>
2023-12-13 12:37:12 +0000

    Inter mistral commit

---
+++ scripts/prepMistral.sh
@@ -0,0 +1,4 @@
+python data_prep/convert_dataset_hf.py \
+  --dataset c4 --data_subset en \
+  --out_root my-copy-c4 --splits train_small val_small \
+  --concat_tokens 1024 --tokenizer meta-llama/Llama-2-7b-hf
---
+++ scripts/trainMistral.sh
@@ -0,0 +1,13 @@
+# composer scripts/train/train.py scripts/train/yamls/pretrain/gpt2-small.yaml \
+#     max_seq_len=2048 \
+#     train_loader.dataset.split=train_small \
+#     eval_loader.dataset.split=val_small \
+rm -rf /workspace/llm-foundry/scripts/mistral
+composer train/train.py \
+  train/yamls/pretrain/mistral.yaml \
+  data_local=my-copy-c4 \
+  train_loader.dataset.split=train_small \
+  eval_loader.dataset.split=val_small \
+  max_duration=100ba \
+  eval_interval=0 \
+  save_folder=mistral
